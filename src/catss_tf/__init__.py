dc0bcbea14d6197f35d8299fda91becbbc796d9a
Oleksandr (Alex) Sosnovshchenko <alex.sosn88@gmail.com>
2026-09-26 10:34:20 +0300

    release: freeze v0.1 public API

--- src/catss_tf/__init__.py
+++ src/catss_tf/__init__.py
@@ -1,5 +1,18 @@
-"""CATSS-TF materializer package."""
+"""Public CATSS-TF v0.1 API."""
 
-__all__ = ["__version__"]
+__version__ = "0.1.0"
 
-__version__ = "0.0.0"
+from catss_tf.bhsa_materializer import materialize_bhsa
+from catss_tf.bhsa_resolver import TextFabricBhsaProvider
+from catss_tf.consistency import compare_projection_bundles
+from catss_tf.lxx_materializer import materialize_lxx
+from catss_tf.lxx_resolver import TextFabricLxxProvider
+
+__all__ = [
+    "__version__",
+    "compare_projection_bundles",
+    "materialize_bhsa",
+    "materialize_lxx",
+    "TextFabricBhsaProvider",
+    "TextFabricLxxProvider",
+]
